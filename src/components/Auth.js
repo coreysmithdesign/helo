73f42aff573ff07be0c1d2352e040bde659c63a7
Corey Smith <corey@coreysmith.design>
2020-06-05 08:31:17 -0700

    first commit

---
+++ src/components/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div>this is the auth component</div>
+    )
+  }
+}
+
+export default Auth
---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <div>this is the Form component</div>
+    )
+  }
+}
+
+export default Form
